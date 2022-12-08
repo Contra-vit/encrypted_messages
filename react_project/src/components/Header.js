0cffc0439fa678c2984748797d2e127a40d16d04
Vladimir Tsaran <vladimirtsaran@MacBook-Air-Vladimir.local>
2022-12-08 13:27:06 +0500

    Final

--- react_project/src/components/Header.js
+++ react_project/src/components/Header.js
@@ -1,6 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
     return (
         <div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink exact className='' to= '/'> Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact className='' to= '/note'> Note</NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact className='' to= '/create'> Create</NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact className='' to= '/about'>About</NavLink>
+                    </li>
+                </ul>
+            </nav>
 
         </div>
     );
--- react_project/src/components/Main.js
+++ react_project/src/components/Main.js
@@ -1,6 +1,12 @@
 function Main() {
     return (
         <div>
+            <div>
+            <a href="/create"> Create note</a>
+            </div>
+            <div>
+                   <a href="/note"> show note</a>    
+            </div>
 
         </div>
     );
